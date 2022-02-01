fff47a6137c721951e575b45c55c029c424fe645
Dalton Ellis <dalton.ellis@trojans.dsu.edu>
2022-01-31 23:42:58 -0600

    updates for lab

--- Graylog-Ship.py
+++ Graylog-Ship.py
@@ -133,7 +133,7 @@ def frameParse(frame):
 	# Dott11Auth
 	# For all frame types, determine the signal strength and include it
 	if frame.haslayer(Dot11Beacon):
-		#pass
+		pass
 		
 		#bf(frame)
 	if frame.haslayer(Dot11Auth):
