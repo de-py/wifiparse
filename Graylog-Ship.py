f845233ea5791f8f6b680010897fc7d84de0d735
Dalton Ellis <dalton.ellis@trojans.dsu.edu>
2022-02-01 22:41:48 -0600

    updates for lab

--- Graylog-Ship.py
+++ Graylog-Ship.py
@@ -38,17 +38,6 @@ class beacFrame:
 
 		return frame_dict
 
-class authFrame:
-	def __init__(self, essid, bssid, ts, oui):
-		self.essid = essid
-		self.bssid = bssid
-		self.ts = ts
-		self.oui = oui
-
-
-	def __str__(self):
-		return "ESSID:{}, BSSID:{}, Timestamp:{}, Vendor OUI:{}".format(self.essid, self.bssid, self.ts, self.oui)
-
 def isMonitor():
 	interfaces = get_if_list()
 	winter = sys.argv[1]
@@ -76,11 +65,11 @@ def isMonitor():
 
 def setMonitor():
 	print("Setting up monitor mode")
-	#time.sleep(2)
+	time.sleep(2)
 	subprocess.run(["ifconfig", sys.argv[1], "down"])	
 	subprocess.run(["iwconfig", sys.argv[1], "mode", "monitor"])
 	subprocess.run(["ifconfig", sys.argv[1], "up"])	
-	#print("Done")
+	print("Done")
 	time.sleep(2)
 	
 
@@ -119,6 +108,7 @@ def bf(frame):
 	return frame_object
 
 
+# Send Frame to Graylog Server as Gelf Format
 def sendFrame(frame_object):
 	gelf_ip = "172.20.4.99"
 	gelf_port = "12201"
@@ -134,10 +124,9 @@ def sendFrame(frame_object):
 	send_dict.update(frame_object.json())
 
 	resp = requests.post(url, json=send_dict)
-	print(resp)
-
+	if DEBUG:
+		print("Server Response:{}".format(resp.status_code))
 def frameParse(frame):
-	# If the frame is a beacon frame (has the beacon layer)
 
 	# Need to add both association and authentication requests/responses
 	# Skipping though because not enough data on campus for testing frames
@@ -145,23 +134,28 @@ def frameParse(frame):
 	# Dot11AssoResp
 	# Dott11Auth
 	# For all frame types, determine the signal strength and include it
+	
+	# If the frame is a beacon frame (has the beacon layer)
 	if frame.haslayer(Dot11Beacon):
+		# bf function will parse Beacon Frame and put into object
 		frame_object = bf(frame)
 		if frame_object:
 			sendFrame(frame_object)
-			#print(frame_object)
+			if DEBUG:
+				print(frame_object)
 	
-		
 
 
 	if frame.haslayer(Dot11Auth):
+		# Putting here in the event we can test later
 		print("dot11auth")
-		#authf(frame)
 
 	if frame.haslayer(Dot11AssoReq):
+		# Putting here in the event we can test later
 		print("assoreq")
 	
 	if frame.haslayer(Dot11AssoResp):
+		# Putting here in the event we can test later
 		print("assoresp")
 
 
@@ -176,6 +170,7 @@ def countdown():
 	time.sleep(1)
 
 
+DEBUG = True
 
 def main():
 	# If interface is not in monitor mode, set it to monitor mode.
@@ -189,7 +184,8 @@ def main():
 	thread.start()
 	
 	# Countdown for dramatic effect and to see what was just displayed before the stream of data.
-	#countdown()
+	if not DEBUG:
+		countdown()
 
 	# Begin to sniff 802.11 frames	
 	sniff(iface=sys.argv[1],prn=frameParse,store=0)
